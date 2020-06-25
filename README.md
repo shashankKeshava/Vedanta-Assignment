40dbb642d87d518b1d4d13a156cd5545698ccead
Shashank K <shashank_k@intuit.com>
2020-06-25 23:00:04 +0530

    Github pages

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Vedanta-Assignment
+
 Created with CodeSandbox
+
+## Getting started
+
+1. `yarn`
+2. `yarn start`
+
+Live Site:
---
+++ build/precache-manifest.2fb146a527d43d6a3cd22b5c881bc2aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d79b2ef4abc29d946f4bd2eafc68e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dd71d7db5eb672f839c",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "799aaacd7a374869a550",
+    "url": "/static/js/2.5f0cb2a0.chunk.js"
+  },
+  {
+    "revision": "3dd71d7db5eb672f839c",
+    "url": "/static/js/main.b5884184.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
